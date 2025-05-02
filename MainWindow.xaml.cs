79dffddff30d129a4453d5a6c3634283dd14bebb
Marcus Liam Nielsen <marcus.email2016@gmail.com>
2025-05-02 21:56:50 +0200

    Added project to GItHub Directory

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace File_Sorter___Organizer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        void BrowseFoldersClick()
+        {
+
+        }
+
+        private void BrowseFoldersClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
